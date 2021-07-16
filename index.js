380afa7ce35d09f0c060418fd7fd3fc7f679b26c
Shirak Avakian <shirakavakian@gmail.com>
2021-07-16 16:35:21 -0700

    mongodb added

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const server = express();
+const mongoose = require("mongoose");
+
 
 //const winston = require("winston");
 const port = process.env.PORT || 3030;
+
+
 
 /*
 const sql = require('mssql')
 */
 server.get("/SayHelloAzure", async (req, res, next) => {
-
-	res.status(200).send("Hello Azure 2");
+	const db = await mongoose.connect("mongodb://ciba-mongodb:pt5tPU56mLdwxlZazOre47ymxOCPsDPhATIcV4NVFEUFLN8GeSgl5Og2GNtD5mCoWWjp4Vz4GOHJAqHnOE2sUg==@ciba-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ciba-mongodb@", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		...{}
+	});
+	if (db)
+		res.status(200).send("Hello Azure 2 database connected");
+	else
+		res.status(200).send("Hello Azure 2 No database connection");
 })
 /*
 server.get("/SayHelloAzure", async (req, res, next) => {
