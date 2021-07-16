37ad46196057be6e632869c2028b77cd54f26872
Shirak Avakian <shirakavakian@gmail.com>
2021-07-16 15:57:40 -0700

    endpoint added

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const port = process.env.PORT || 3030;
 
 /*
 const sql = require('mssql')
+*/
+server.get("/SayHelloAzure", async (req, res, next) => {
 
-
-
+	res.status(200).send("Hello Azure");
+})
+/*
 server.get("/SayHelloAzure", async (req, res, next) => {
 	await sql.connect('Server=demo-sqldbserver-sa.database.windows.net,1433;Database=Users;User Id=savakian;Password=Monday11?; Encrypt = true')
 	const result = await sql.query(`select * from employee`)
